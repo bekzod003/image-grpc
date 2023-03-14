a26a5e2771be3fe10a421d3a96430a758c573335
bekzod003 <begiobm@gmail.com>
2023-03-14 11:51:42 +0500

    run app

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"github.com/bekzod003/image-grpc/config"
+	"github.com/bekzod003/image-grpc/internal/app"
+)
+
 func main() {
 	// Run the server
+	cfg := config.GetConfig()
+	app.Run(cfg)
 }
